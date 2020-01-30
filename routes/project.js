437a709a809e5eb27871264cc3b1a1c7660123c0
Meghana Sridhar <mesridha@ucsd.edu>
2020-01-29 17:50:57 -0800

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.viewProject = function(req, res){
+  var name = req.params.name; 
+  console.log("The project name is: " + name);
+  res.render('project', {
+    'projectName': name
+  });
+}
