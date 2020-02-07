77a59440cc3e74a162270a3afcdbae0de5a56b07
Meghana Sridhar <mesridha@ucsd.edu>
2020-02-06 18:56:42 -0800

    Somewhat works!

--- routes/index.js
+++ routes/index.js
@@ -6,37 +6,17 @@
 exports.view = function(req, res){
   res.render('index', {
     'projects': [
-      { 'name': 'Waiting in Line',
-        'image': 'lorempixel.people.1.jpeg',
-        'id': 'project1'
+      { 'name': 'CSE 170',
+        'image': 'CSE 170.jpeg',
+        'id': '1'
       },
-      { 'name': 'Needfinding',
-        'image': 'lorempixel.city.1.jpeg',
-        'id': 'project2'
+      { 'name': 'CSE 190',
+        'image': 'CSE 190.jpeg',
+        'id': '2'
       },
-      { 'name': 'Prototyping',
-        'image': 'lorempixel.technics.1.jpeg',
-        'id': 'project3'
-      },
-      { 'name': 'Heuristic Evaluation',
-        'image': 'lorempixel.abstract.1.jpeg',
-        'id': 'project4'
-      },
-      { 'name': 'Visualization',
-        'image': 'lorempixel.abstract.8.jpeg',
-        'id': 'project5'
-      },
-      { 'name': 'Social design',
-        'image': 'lorempixel.people.2.jpeg',
-        'id': 'project6'
-      },
-      { 'name': 'Gestural interaction',
-        'image': 'lorempixel.technics.2.jpeg',
-        'id': 'project7'
-      },
-      { 'name': 'Design tools',
-        'image': 'lorempixel.city.2.jpeg',
-        'id': 'project8'
+      { 'name': 'ENG 100D',
+        'image': 'ENG 100D.jpeg',
+        'id': '3'
       }
     ]  
   });
